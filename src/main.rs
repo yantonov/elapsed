d23f680e6d70dcfeefe4b769eea7a9d194e43a9e
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2020-12-20 10:45:40 +0100

    pretty print error message

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,55 @@
 use crate::cli::Command;
-use chrono::{Utc, TimeZone};
+use chrono::{Utc, TimeZone, NaiveDate};
+use colored::Colorize;
 
 mod cli;
 mod elapsed;
 
-fn main() {
+fn get_from(date: &String) -> Result<NaiveDate, String> {
+    let parsed_from_date = Utc.datetime_from_str(
+        &format!("{} 00:00:00", date),
+        "%Y-%m-%d %H:%M:%S")
+        .map_err(|_| "Date should follow the YYYY-MM-DD format".to_string())?;
+    Ok(parsed_from_date
+        .date()
+        .naive_local())
+}
+
+fn get_to(now: &Option<String>) -> Result<NaiveDate, String> {
+    Ok(match now {
+        None => Utc::now().date().naive_local(),
+        Some(now_value) => {
+            let parsed_to_date = Utc.datetime_from_str(
+                &format!("{} 00:00:00", now_value),
+                "%Y-%m-%d %H:%M:%S")
+                .map_err(|_| "Date should follow the YYYY-MM-DD format".to_string())?;
+            parsed_to_date
+                .date()
+                .naive_local()
+        }
+    })
+}
+
+fn entry_point() -> Result<(), String> {
     let arguments = cli::arguments();
     match arguments.command() {
         Command::Since(since) => {
-            let from = Utc.datetime_from_str(
-                &format!("{} 00:00:00", since.date),
-                "%Y-%m-%d %H:%M:%S")
-                .expect("Date should follow the YYYY-MM-DD format")
-                .date()
-                .naive_local();
-            let to = match &since.now {
-                None => Utc::now().date().naive_local(),
-                Some(to_value) => {
-                    Utc.datetime_from_str(
-                        &format!("{} 00:00:00", to_value),
-                        "%Y-%m-%d %H:%M:%S")
-                        .expect("Date should follow the YYYY-MM-DD format")
-                        .date()
-                        .naive_local()
-                }
-            };
+            let from = get_from(&since.date)?;
+            let to = get_to(&since.now)?;
             println!("{}", elapsed::elapsed(&from, &to).unwrap().to_string());
+        }
+    }
+    Ok(())
+}
+
+fn main() {
+    match entry_point() {
+        Ok(_) => {
+            std::process::exit(0);
+        }
+        Err(message) => {
+            eprintln!("{} {}", "[ERROR]".red(), message);
+            std::process::exit(1);
         }
     }
 }
